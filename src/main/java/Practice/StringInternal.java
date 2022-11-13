a594b4936114a05b627957b11c1ae97fd8bf2f01
RajaPrabhuP <p.rajaprabhu.be@gmail.com>
2022-11-13 11:43:12 +0530

    Good Pairs Formula solution is added

---
+++ src/main/java/Practice/StringInternal.java
@@ -0,0 +1,27 @@
+package Practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StringInternal {
+
+	public static void main(String[] args) {
+		char[] chArr = new char[10];
+		String str = new String("ABCDEF");
+		int codePointAt = str.codePointBefore(1);
+		str.getChars(0, 2, chArr, 0);
+		System.out.println(str);
+		System.out.println(codePointAt);
+		System.out.println(str.hashCode());
+		
+		str.getChars(0, 3 , chArr, 5);
+		System.out.println(Arrays.toString(chArr));
+		ArrayList<String> al = new ArrayList<>(); 
+		al.add("A");
+		al.add("B");
+		al.add("C");
+		System.out.println(Arrays.toString(al.toArray()));
+		System.out.println(Arrays.toString(al.subList(0, al.size()).toArray()));
+	}
+
+}
--- src/main/java/hashing/P003_NoOfGoodPairs.java
+++ src/main/java/hashing/P003_NoOfGoodPairs.java
@@ -17,6 +17,13 @@ public class P003_NoOfGoodPairs {
 		
 	}
 	
+	/*
+	 * Using HashMap and identify the number of occurence of each elm in array
+	 * And get the values from hmap 
+	 * and add the sum with each number of fact(+) for each number and sum.
+	 * 
+	 * */
+	
 	 public static int numIdenticalPairsMap(int[] nums) {
 		
 		 HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
@@ -36,9 +43,50 @@ public class P003_NoOfGoodPairs {
 	        
 	    }
 	
+	 /*Brute Force
+	  * 
+	  * Iterate through 2 for loops
+	  * outer loop starts with zero and Inner for loop j = i+1
+	  * and if arr[i] and arr[j] are equal means increment the counter.
+	  * 
+	  * */
+	 public static int numIdenticalPairsBruteForce(int[] nums) {
+			int counter = 0;
+			 
+			 for(int i = 0; i < nums.length; i++) {
+				 
+				 for(int j = i + 1; j < nums.length; j++) {
+					 
+					 if(nums[i] == nums[j])
+						 counter++;				 
+				 }
+				 
+			 }
+			 System.out.println(counter);
+			 return counter;	
+		    }
+	 
+	 public static int numIdenticalPairsUsingFormula(int[] nums) {
+			
+		 HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+		 for (int i : nums) 
+			hmap.put(i, hmap.getOrDefault(i, 0) + 1);
+				 
+		 int sum = 0;
+		  for (int i : hmap.values()) 			 
+			  sum=+ (i*(i - 1)/ 2);
+			
+		  System.out.println(sum);
+		  return sum;
+	        
+	    }
+	 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		numIdenticalPairsMap(new int[] {1,2,3});
+		numIdenticalPairsBruteForce(new int[] {1,1,1,1,1});
+		numIdenticalPairsUsingFormula(new int[] {1,1,1,1,1});
 	}
 
 }
